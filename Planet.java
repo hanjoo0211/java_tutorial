b11e2dc34db5c5fde7dfa5722666683cc99a09a8
hanjoo0211 <hanjoo0211@gmail.com>
2024-11-23 22:38:08 +0900

    Section 5

---
+++ Planet.java
@@ -0,0 +1,11 @@
+class Planet {
+    void revolve() {
+        System.out.println("Revolving...");
+    }
+
+    public static void main(String[] args) {
+        Planet planet = new Planet();
+        planet.revolve();    
+    }
+}
+
